edbc33e83e4b04cf14749b01121fcd6bccb38153
martyste <martyste@student.ethz.ch>
2020-09-19 23:31:58 +0200

    add ingredients

--- app/api/v1/ingredient.py
+++ app/api/v1/ingredient.py
@@ -1,11 +1,16 @@
 from flask import jsonify
 from flask_cors import cross_origin
 from flask_restplus import Namespace, Resource
+from elasticsearch import Elasticsearch
+
 
 from app.database.models import ShoppingList
 
 ns = Namespace('ingredient')
 
+es = Elasticsearch(
+    ['https://hackzurich-api.migros.ch/hack/recipe/recipes_de/_search'],
+    http_auth=('hackzurich2020', 'uhSyJ08KexKn4ZFS'))
 
 @ns.route('/')
 class IngredientResource(Resource):
@@ -36,8 +41,32 @@ class IngredientResource(Resource):
             :param recipe_ids: List of recipe ids in the shopping list
             :return: List of ingredients
             """
-            # TODO: Insert code
-            return recipe_ids
+            params = {"query": {
+                "ids": {
+                    "values": recipe_ids
+                }
+            }
+            }
+            indxs_request = es.search(index='recipes_de', body=params)
+            recipes = indxs_request['hits']['hits']
+
+            ingredients = {}
+            ingredients['test'] = 'hi'
+            for recipe in recipes:
+                factor = 1
+                recipe = recipe['_source']
+                size = recipe['available_sizes'][0]
+                if size != 2:
+                    factor = size / 2
+                for ingredient in recipe['sizes'][0]['ingredient_blocks'][0]['ingredients']:
+                    if ingredient['text'] in ingredients:
+                        ingredients[ingredient['text']]['amount'] += ingredient['amount']['quantity'] / factor
+                    else:
+                        ingredients[ingredient['text']] = {}
+                        ingredients[ingredient['text']]['amount'] = ingredient['amount']['quantity'] / factor
+                        ingredients[ingredient['text']]['unit'] = ingredient['amount']['unit']
+
+            return ingredients
 
         recipe_ids = get_shopping_list_recipe_ids()
         ingredients = get_ingredients(recipe_ids)
--- app/utils/recipe_parser.py
+++ app/utils/recipe_parser.py
@@ -2,7 +2,12 @@ def extract_recipe_info(recipe):
     recipe = recipe['_source']
     image_1_1 = recipe['images'][0]['ratios'][4]['stack'].replace('{stack}', 'medium')
     image_16_9 = recipe['images'][0]['ratios'][1]['stack'].replace('{stack}', 'medium')
-
+    size = recipe['available_sizes'][0]
+    if size != 2:
+        factor = size / 2
+        for i, ingredient in enumerate(recipe['sizes'][0]['ingredient_blocks'][0]['ingredients']):
+            recipe['sizes'][0]['ingredient_blocks'][0]['ingredients'][i]['amount']['quantity'] = \
+                recipe['sizes'][0]['ingredient_blocks'][0]['ingredients'][i]['amount']['quantity'] / factor
     return {
         'id': recipe['id'],
         'title': recipe['title'],
