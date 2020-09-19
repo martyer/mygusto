1717fea038eacd79125b2880a063a28a0eaa121f
martyste <martyste@student.ethz.ch>
2020-09-19 20:52:04 +0200

    Add image link

--- app/api/v1/recipe.py
+++ app/api/v1/recipe.py
@@ -37,13 +37,17 @@ liked_input = ns.model('liked_input', {
 
 def extract_recipe_info(recipe):
     recipe = recipe['_source']
+    image_1 = recipe['images'][0]['ratios'][4]['stack'].replace('{stack}', 'medium')
+    image_16 = recipe['images'][0]['ratios'][1]['stack'].replace('{stack}', 'medium')
+
     return {'id': recipe['id'],
             'title': recipe['title'],
             'teasertext': recipe["teasertext"],
             'duration': recipe["duration_total_in_minutes"],
             "steps": recipe['steps'],
             "nutrients": recipe['nutrients'],
-            "ingredients": recipe["sizes"][0]["ingredient_blocks"][0]["ingredients"]
+            "ingredients": recipe["sizes"][0]["ingredient_blocks"][0]["ingredients"],
+            "images": {'1:1': image_1, '16:9': image_16}
             }
 
 
